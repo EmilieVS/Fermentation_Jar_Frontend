303ac8469b8922625935ed2696a72e34adff77f4
chazera <hazera.c@gmail.com>
2025-07-24 17:47:09 +0200

    update: list of posts working

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -2,13 +2,21 @@
     <div class="flex flex-col">
         <div class="block mb-2">
             <p class="text-white-snow">{{ props.displayName }}</p>
-            <p class="text-gray font-light italic">{{ props.username }}</p>
+            <p class="text-gray font-light italic">@{{ props.username }}</p>
         </div>
         <div class="block bg-regular-blue p-2 rounded-xl">
             <p class="text-white-snow">{{ props.description }}</p>
         </div>
         <div class="flex justify-end">
-            <p class="text-gray text-sm italic">{{ props.createdAt }}</p>
+            <p class="text-gray text-sm italic">
+                {{ new Date(props.createdAt).toLocaleString('fr-FR', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit'
+                }) }}
+            </p>
         </div>
     </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,6 @@
 <template>
-    <div class="flex flex-col justify-center items-center h-screen bg-dark-blue">
-        <header class="flex flex-col justify-center items-center">
-            <img src="../assets/logos/fermentation_jar_logo_light.png" alt="Brand Logo Light" class="w-40 h-40">
-            <p class="text-gray">Cultivate community.</p>
-        </header>
-        <section class="absolute top-3 right-3 text-white-snow">
+    <div class="flex flex-col justify-center items-center min-h-screen bg-dark-blue pt-4 pb-4">
+        <section class="ml-auto mr-4 text-white-snow ">
             <div v-if="auth.token" class="flex justify-center items-center gap-2">
                 <p>Hello {{ auth.displayName }}</p>
                 <img src="../assets/icons/account_icon.png" alt="Profile Icon Light" class="w-10 h-10">
@@ -15,10 +11,14 @@
                 <RouterLink to="/signup">Sign Up</RouterLink>
             </div>
         </section>
-        <section class="m-4 w-4/5 lg:w-1/2">
-            <div class="flex flex-col gap-12">
+        <header class="flex flex-col justify-center items-center">
+            <img src="../assets/logos/fermentation_jar_logo_light.png" alt="Brand Logo Light" class="w-30 h-30">
+            <p class="text-gray">Cultivate community.</p>
+        </header>
+        <section class="pt-4 w-4/5 lg:w-1/2">
+            <div class="flex flex-col gap-8">
                 <PostItem v-for="post in postList" :id="post.id" :displayName="post.displayName"
-                    :username="post.username" :description="post.description" :createdAt="post.createdAt" />
+                    :username="post.username" :description="post.description" :createdAt="post.created_at" />
             </div>
         </section>
     </div>
@@ -31,23 +31,6 @@ import { ref, onMounted } from 'vue';
 
 const auth = useAuthStore();
 const postList = ref([]);
-
-postList.value = [
-    {
-        id: 1,
-        displayName: 'Lolo le zozo',
-        username: '@nadrojria',
-        description: 'Feeling like picking some cabbages today !',
-        createdAt: '15/07/2025',
-    },
-    {
-        id: 2,
-        displayName: 'Houmous man',
-        username: '@hazbraaaa',
-        description: 'Made awesome cucumber kimchi yesterday.',
-        createdAt: '18/07/2025',
-    },
-]
 
 function loadAllPosts() {
     fetch("http://localhost:8000/api/posts/all", {
