5e892e6eaa4a2e65711dd5e2e9a03045b78a5e85
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-07-25 15:59:36 +0200

    update: profile page done

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,36 +1,41 @@
 <template>
 
     <div class="flex flex-col justify-center items-center min-h-screen bg-dark-blue pt-4 pb-4">
-          <div class="absolute top-3 left-3 h-10 w-10">
+        <div class="absolute top-3 left-3 h-10 w-10">
             <RouterLink to="/home">
                 <img src="../assets/icons/back_arrow_button.png" alt="Back Arrow Button" class="">
             </RouterLink>
         </div>
         <section class="ml-auto mr-4 text-white-snow absolute top-4 right-4">
-            <div v-if="auth.token" class="flex justify-center items-center gap-2">
+            <div class="flex justify-center items-center gap-2">
                 <p>Hello {{ auth.displayName }}</p>
                 <img src="../assets/icons/account_icon.png" alt="Profile Icon Light" class="w-10 h-10">
             </div>
         </section>
-        <header class="flex flex-col justify-center items-center">
-            <img src="../assets/logos/fermentation_jar_logo_light.png" alt="Brand Logo Light" class="w-30 h-30">
-            <p class="text-gray">Cultivate community.</p>
-        </header>
         <section class="pt-4 w-4/5 lg:w-1/2 text-white-snow">
-            <div class="flex flex-col gap-8">
-                <div>{{userInfos[0].user.displayName}}</div>
-                <div>{{userInfos[0].user.username}}</div>
-                <div>{{userInfos[0].user.bio}}</div>
+            <div class="flex flex-col gap-8" v-if="userInfos.user">
+                <div class="flex justify-between  items-center w-full">
+                    <div class="flex flex-col">
+                        <div class="text-2xl">{{ userInfos.user.displayName }}</div>
+                        <div class="text-gray">@{{ userInfos.user.username }}</div>
+                    </div>
+                    <div class="flex justify-center">
+                        <button type="submit"
+                            class="bg-light-blue text-dark-blue font-semibold p-1 rounded-xl px-15 flex justify-center">
+                            Edit profile
+                        </button>
+                    </div>
+                </div>
+                <div class="pb-5">{{ userInfos.user.bio }}</div>
             </div>
-            <!-- </div> -->
+            <hr class="border-gray">
         </section>
-
-        <!-- <section class="pt-4 w-4/5 lg:w-1/2">
+        <section class="pt-4 w-4/5 lg:w-1/2">
             <div class="flex flex-col gap-8">
                 <PostItem v-for="post in usersPost" :username="post.username" :description="post.description"
                     :createdAt="post.created_at" />
             </div>
-        </section> -->
+        </section>
 
     </div>
 
@@ -44,21 +49,21 @@ import { ref, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 
 const auth = useAuthStore();
-const userInfos = ref([]);
+const userInfos = ref({});
 const usersPost = ref([]);
 
 
 function getUserData() {
     fetch(`http://localhost:8000/api/users`, {
         method: 'GET',
-        headers: {Authorization : `Bearer ${auth.token}`} 
+        headers: { Authorization: `Bearer ${auth.token}` }
     })
         .then(response => response.json())
-    
+
         .then(data => {
             userInfos.value = data;
             console.log(data)
-            
+
         })
         .catch(error => {
             console.log(error);
@@ -69,7 +74,7 @@ function getUserData() {
 function loadUserPosts() {
     fetch("http://localhost:8000/api/posts", {
         method: 'GET',
-        headers: {Authorization : `Bearer ${auth.token}`} 
+        headers: { Authorization: `Bearer ${auth.token}` }
     })
         .then(response => response.json())
         .then(data => {
@@ -82,7 +87,7 @@ function loadUserPosts() {
 
 onMounted(() => {
     getUserData();
-    // loadUserPosts();
+    loadUserPosts();
 })
 
 
